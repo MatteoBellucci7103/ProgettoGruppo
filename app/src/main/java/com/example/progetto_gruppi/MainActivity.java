afcbc673063321ca337a2de7c212513137cc6b62
matteo <matteobellucci01@gmail.com>
2021-12-02 11:43:23 +0100

    Push sul ramo con Toast

--- app/src/main/java/com/example/progetto_gruppi/MainActivity.java
+++ app/src/main/java/com/example/progetto_gruppi/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.progetto_gruppi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Context context = getApplicationContext();
+        CharSequence text = "Sono un toast!";
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
